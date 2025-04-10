c7bb23f46a8790ff76d20382af7aea221b48d4b4
Mr.Xbott <mr.xbott@gmail.com>
2025-04-10 17:16:24 +0300

    subscriptions_feed page

--- mediajournal/web/urls.py
+++ mediajournal/web/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('search/', views.search, name='search'),
+    path('feed/', views.subscriptions_feed, name='subscriptions_feed'),
 ]
--- mediajournal/web/views.py
+++ mediajournal/web/views.py
@@ -1,10 +1,13 @@
+from django.db.models import Count, F, Value, Q
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib.postgres.search import SearchVector
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import redis
 
 from articles.models import Article, Category, ArticleSection
+from news.models import News
 from .forms import SearchForm
 
 r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
@@ -20,13 +23,41 @@ def main_page(request):
         })
 
 
+@login_required
+def subscriptions_feed(request):
+    users = request.user.following.all()
+    all_articles = Article.objects.filter(author__in=users, status=Article.Status.PUBLISHED)\
+                                .select_related('author', 'category')\
+                                .order_by('-published')\
+                                .annotate(
+                                    bookmarks_count=Count('bookmarked_by'), 
+                                    comments_count=Count('article_comments', filter=Q(article_comments__is_active=True))
+                                    )
+    
+    page = request.GET.get('page')
+    paginator = Paginator(all_articles, 3)
+    articles = []
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
+    total_views = {article.id: int(r.get(f'article:{article.id}:views')) if r.get(f'article:{article.id}:views') else 0 for article in articles}
+    return render(request, 'subscriptions_feed.html', {'section': 'subscriptions_feed',
+                                                       'articles': articles,
+                                                       'total_views': total_views,
+                                                       })
+
+
 def search(request):
     query = None
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            article_search_vector = SearchVector('title', 'body', 'author__username', config='russian')
+            article_search_vector = SearchVector('title', 'text', 'author__username', config='russian')
             article_results = Article.objects.filter(status=Article.Status.PUBLISHED).annotate(search=article_search_vector).filter(search=query).order_by('-published')
             section_search_vector = SearchVector('title', 'text', 'quote', config='russian')
             article_ids_from_sections = ArticleSection.objects.filter(article__status=Article.Status.PUBLISHED).annotate(search=section_search_vector).filter(search=query).values_list('article__id', flat=True)
