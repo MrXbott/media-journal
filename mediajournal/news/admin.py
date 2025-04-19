681030e22a9c3d69cf218ceeb3ddc2e4367673bb
Mr.Xbott <mr.xbott@gmail.com>
2025-04-19 15:12:12 +0300

    change displayed fields

--- mediajournal/news/admin.py
+++ mediajournal/news/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'status']
+    list_display = ['title', 'status', 'published', 'author', ]
     fields = ['title', 'status', 'author', 'text', 'published', 'cover', 'cover_preview']
     readonly_fields = ['cover_preview']
