be6e6d5c1b7e586ebc79db2ca64fb803e715b563
Mr.Xbott <mr.xbott@gmail.com>
2025-04-21 18:55:02 +0300

    remove unused code from bookmarks views

--- mediajournal/bookmarks/views.py
+++ mediajournal/bookmarks/views.py
@@ -1,18 +1,14 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from django.contrib.contenttypes.models import ContentType
 
 from .models import Bookmark
-from articles.models import Article
-from news.models import News
 
 
 @login_required
 @require_POST
 def add_remove_bookmark(request):
-    # article_id = request.POST.get('article_id')
     object_id = request.POST.get('object_id')
     content_type_name = request.POST.get('content_type')
     try:
@@ -20,14 +16,6 @@ def add_remove_bookmark(request):
         content_object = content_type.get_object_for_this_type(id=object_id)
     except:
         return JsonResponse({'status': 'error', 'message': 'wrong content type or object id'})
-    # user_bookmarks = Bookmark.objects.filter(user=request.user).values_list('content_object', flat=True)
-    print('------', content_type, content_object)
-    print(request.user)
-    print(request.user.bookmarks.all())
-    # try:
-    #     article = Article.objects.get(id=article_id)
-    # except Article.DoesNotExist:
-    #     return JsonResponse({'status': 'error', 'message': 'article doesn\'t exist'})
     
     if Bookmark.objects.filter(user=request.user, object_id=object_id, content_type=content_type).exists():
         Bookmark.objects.filter(user=request.user, content_type=content_type, object_id=object_id).delete()
@@ -35,5 +23,3 @@ def add_remove_bookmark(request):
     else:
         Bookmark.objects.create(user=request.user, content_type=content_type, object_id=object_id)
         return JsonResponse({'status': 'ok', 'bookmark': 'added'})
-
-    # return JsonResponse()
